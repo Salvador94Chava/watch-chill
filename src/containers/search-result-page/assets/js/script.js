cf61ace0a390e139bb2d02021c24c74666a91800
Regulo Argotte <rargotte@hotmail.com>
2021-12-21 19:12:30 -0600

    Changing carousel size to poster view

--- src/containers/search-result-page/assets/js/script.js
+++ src/containers/search-result-page/assets/js/script.js
@@ -1,7 +1,15 @@
 const APIKey = "612305371d20a404c362f2768ab01667";
 
-/*genero*/
-/*año*/
+var queryURL2 = "https://api.themoviedb.org/3/discover/movie?api_key=b574c405150412e74904c09bd2c259b6&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate";
+
+
+
+
+function readLocalStorage() {
+
+    
+}
+
 function addMovie() {
     var sliderEl = document.getElementById("slider1");
     var cardEl = document.createElement("div");
@@ -10,9 +18,9 @@ function addMovie() {
     card_imageEl.setAttribute("class", "card-image");
 
     var figureEl = document.createElement("figure");
-    figureEl.setAttribute("class", "image is-16by9 is-covered");
+    figureEl.setAttribute("class", "image is-3by4 is-covered");
     var imgEl = document.createElement("img");
-    imgEl.setAttribute("src", "https://images.unsplash.com/photo-1550945771-515f118cef86?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80");
+    imgEl.setAttribute("src", "https://image.tmdb.org/t/p/w185/1g0dhYtq4irTY1GPXvft6k4YLjm.jpg");
     imgEl.setAttribute("alt", "");
     figureEl.append(imgEl);
 
@@ -36,6 +44,21 @@ function addMovie() {
 
     sliderEl.append(cardEl);
 }
+
+fetch(queryURL2)
+    .then(function (response) {
+
+        if (response.status === 200) {
+
+        }
+        return response.json();
+    })
+    .then(function (data) {
+
+        console.log(data);
+
+    })
+
 
 addMovie();
 

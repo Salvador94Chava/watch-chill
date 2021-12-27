655b01984d5f018b3a614126b8146f3ed7c60b48
Guillermo Lazo <glazovg@gmail.com>
2021-12-27 15:07:23 -0600

    Refactoring api call to get keyword ids

--- src/containers/extra-options/js/local-storage.js
+++ src/containers/extra-options/js/local-storage.js
@@ -2,31 +2,21 @@ const API_KEY = "612305371d20a404c362f2768ab01667";
 var keywordInput = document.querySelector("#keyword input");
 
 function saveKeywordIds() {
-    var url = `https://api.themoviedb.org/3/search/keyword?api_key=${API_KEY}&query=${keywordInput.value}`;
-    console.log("Sending request: " + url);
-    var xhr = new XMLHttpRequest();
+    if (keywordInput.value === '') return;
     var keywordIds = [];
-    
-    xhr.open("GET", url, true);
-    xhr.onload = function (e) {
-        if (this.readyState === 4) {
-            var localS = JSON.parse(this.responseText);
-            for (var i = 0; i < localS.results.length; i++) {
-                console.log(localS.results[i].id);
-                if (i <= 6)
-                    keywordIds.push(localS.results[i].id);
+    var url = `https://api.themoviedb.org/3/search/keyword?api_key=${API_KEY}&query=${keywordInput.value}`;
+
+    fetch(url)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            for (var i = 0; i < data.results.length; i++) {
+                keywordIds.push(data.results[i].id);
             }
             localStorage.setItem("keywords", keywordIds);
-        } else {
-            //If there is an error is going to be printed into console.
-            console.error(xhr.statusText);
-        }
-    };
-
-    xhr.send();
-    xhr.onerror = function (e) {
-        new Error(xhr.statusText);
-    };
+        })
 };
 
 function saveToLocalStorage() {
@@ -34,7 +24,7 @@ function saveToLocalStorage() {
 
     let index = languages.findIndex(language => language.English === languagesMenu.value);
     let alpha = languages[index].alpha2;
-    
+
     localStorage.setItem('language', alpha);
     localStorage.setItem('fromYear', fromYearMenu.value);
     localStorage.setItem('toYear', toYearMenu.value);

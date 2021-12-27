74046129b67d57f18425d037623a23a260aed7ce
Guillermo Lazo <glazovg@gmail.com>
2021-12-27 14:37:37 -0600

    Adding saveToLocalStorage method

--- src/containers/extra-options/js/local-storage.js
+++ src/containers/extra-options/js/local-storage.js
@@ -1,11 +1,7 @@
 const API_KEY = "612305371d20a404c362f2768ab01667";
 var keywordInput = document.querySelector("#keyword input");
 
-function outputKeyword() {
-    var selectInput = document.getElementById("input-select");
-}
-
-function apiRequest() {
+function saveKeywordIds() {
     var url = `https://api.themoviedb.org/3/search/keyword?api_key=${API_KEY}&query=${keywordInput.value}`;
     console.log("Sending request: " + url);
     var xhr = new XMLHttpRequest();
@@ -15,16 +11,14 @@ function apiRequest() {
     xhr.onload = function (e) {
         if (this.readyState === 4) {
             var localS = JSON.parse(this.responseText);
-            console.log(localS.results);
-            console.log(this.response);
             for (var i = 0; i < localS.results.length; i++) {
                 console.log(localS.results[i].id);
                 if (i <= 6)
                     keywordIds.push(localS.results[i].id);
             }
             localStorage.setItem("keywords", keywordIds);
-
         } else {
+            //If there is an error is going to be printed into console.
             console.error(xhr.statusText);
         }
     };
@@ -35,4 +29,15 @@ function apiRequest() {
     };
 };
 
-searchButton.addEventListener('click', apiRequest)
+function saveToLocalStorage() {
+    saveKeywordIds();
+
+    let index = languages.findIndex(language => language.English === languagesMenu.value);
+    let alpha = languages[index].alpha2;
+    
+    localStorage.setItem('language', alpha);
+    localStorage.setItem('fromYear', fromYearMenu.value);
+    localStorage.setItem('toYear', toYearMenu.value);
+}
+
+searchButton.addEventListener('click', saveToLocalStorage);

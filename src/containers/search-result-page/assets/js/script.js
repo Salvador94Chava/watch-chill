efe73b29900703f18e4b04f372ae2db600fcc51f
Regulo Argotte <rargotte@hotmail.com>
2021-12-29 12:06:55 -0600

    Adding logic for no results found

--- src/containers/search-result-page/assets/js/script.js
+++ src/containers/search-result-page/assets/js/script.js
@@ -1,13 +1,18 @@
 const APIKey = "612305371d20a404c362f2768ab01667";
 
+
 var yesGenresLocalStorage = [""];
 var noGenresLocalStorage = [""];
 var keywords = "";
-var language = "";
+var language = "en-US";
 var moviesArray = [""];
 var genresArray = [""];
+var today = moment();
+var toYear = moment(today).format("YYYY");
+var fromYear = toYear;
 
-var queryURL = "https://api.themoviedb.org/3/discover/movie?api_key=b574c405150412e74904c09bd2c259b6&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate";
+
+var queryURL = "";
 var genresQueryURL = "https://api.themoviedb.org/3/genre/movie/list?api_key=" + APIKey + "&language=en-US";
 
 function readLocalStorage() {
@@ -15,17 +20,49 @@ function readLocalStorage() {
     noGenresLocalStorage = JSON.parse(localStorage.getItem("noGenres"));
     keywords = localStorage.getItem("keywords");
     language = localStorage.getItem("language");
-    console.log("yesGenres: " + yesGenresLocalStorage);
-    console.log("NoGenres: " + noGenresLocalStorage);
-    console.log("Keywords: " + keywords);
-    console.log("Language: " + language);
+}
 
+function arrayToString(myArray) {
+    var myString = "";
+    for (var index = 0; index < myArray.length; index++) {
+        var element = myArray[index];
+        if (!myString) {
+            myString = element;
+        } else
+            myString = myString + "%2C" + element;
+    }
+    return (myString);
+}
+
+function buildQueryURL() {
+    queryURL = "https://api.themoviedb.org/3/discover/movie?api_key=" + APIKey;
+
+    language = "&language=" + language;
+    queryURL = queryURL + language;
+
+    var sort = "&sort_by=popularity.desc&include_adult=false&include_video=false&page=1";
+    queryURL = queryURL + sort;
+
+    if (yesGenresLocalStorage) {
+        var yesGenres = arrayToString(yesGenresLocalStorage);
+        yesGenres = "&with_genres=" + yesGenres;
+        queryURL = queryURL + yesGenres;
+    }
+    if (noGenresLocalStorage) {
+        var noGenres = arrayToString(noGenresLocalStorage);
+        noGenres = "&without_genres=" + noGenres;
+        queryURL = queryURL + noGenres;
+    }
+    if (keywords) { }
+    keywords = "&with_keywords=" + keywords.replace(/,/g, "%2C");
+    queryURL = queryURL + keywords;
 }
 
 function getMoviesFromAPI() {
-    /* Get the list of Genres*/
     readLocalStorage();
+    buildQueryURL();
     fetch(genresQueryURL)
+        /* Get the list of Genres*/
         .then(function (response) {
             if (response.status === 200) {
 
@@ -51,22 +88,35 @@ function getMoviesFromAPI() {
         .then(function (data) {
 
             readMoviesArray(data.results);
+            console.log(queryURL)
 
         })
 
         .then(function (data) {
-
             addBulmaMain();
+
         })
 
 }
+
+function displayNoResultSlides() {
+
+
+
+};
+
 function readMoviesArray(myArray) {
-    for (var index = 0; index < myArray.length; index++) {
-        var movie = myArray[index];
-        addMovie(movie);
+    if (myArray.length == 0) {
+
+        for (var index = 0; index < 6; index++) {
+            addMovie(null);
+        }
+    } else {
+        for (var index = 0; index < myArray.length; index++) {
+            var movie = myArray[index];
+            addMovie(movie);
+        }
     }
-
-
 }
 
 function translateGenres(myGenres) {
@@ -111,38 +161,48 @@ function addBulmaMain() {
 function addMovie(myMovie) {
     var sliderEl = document.getElementById("slider1");
     var cardEl = document.createElement("div");
-    cardEl.setAttribute("class", "card " + myMovie.id);
+    if (!myMovie)
+        cardEl.setAttribute("class", "card");
+    else
+        cardEl.setAttribute("class", "card " + myMovie.id);
     var card_imageEl = document.createElement("div");
     card_imageEl.setAttribute("class", "card-image");
 
     var figureEl = document.createElement("figure");
-    figureEl.setAttribute("class", "image is-16by9 is-covered");
+    figureEl.setAttribute("class", "image is-5by3 is-covered");
     var imgEl = document.createElement("img");
-    var imgLink = "https://image.tmdb.org/t/p/w500" + myMovie.backdrop_path;
+    if (!myMovie)
+        var imgLink = "https://media.istockphoto.com/vectors/no-result-not-found-or-404-web-page-error-illustration-vector-id846795366";
+    else
+        var imgLink = "https://image.tmdb.org/t/p/w500" + myMovie.backdrop_path;
     imgEl.setAttribute("src", imgLink);
-    imgEl.setAttribute("alt", myMovie.title);
+    if (!myMovie)
+        imgEl.setAttribute("alt", "no results");
+    else
+        imgEl.setAttribute("alt", myMovie.title);
     figureEl.append(imgEl);
 
     card_imageEl.append(figureEl);
+    if (myMovie) {
+        var card_contentEl = document.createElement("div");
+        card_contentEl.setAttribute("class", "card-content");
+        var item_titleEl = document.createElement("div");
+        item_titleEl.setAttribute("class", "item__title");
+        item_titleEl.textContent = adaptText(myMovie.title, 41);
+        var item_descriptionEl = document.createElement("div");
+        item_descriptionEl.setAttribute("class", "item__description");
+        item_descriptionEl.textContent = adaptText(myMovie.overview, 82);
+        var item_genresEl = document.createElement("div");
+        item_genresEl.setAttribute("class", "item__description");
+        item_genresEl.textContent = "Genre: " + translateGenres(myMovie.genre_ids);
 
-    var card_contentEl = document.createElement("div");
-    card_contentEl.setAttribute("class", "card-content");
-    var item_titleEl = document.createElement("div");
-    item_titleEl.setAttribute("class", "item__title");
-    item_titleEl.textContent = adaptText(myMovie.title, 41);
-    var item_descriptionEl = document.createElement("div");
-    item_descriptionEl.setAttribute("class", "item__description");
-    item_descriptionEl.textContent = adaptText(myMovie.overview, 82);
-    var item_genresEl = document.createElement("div");
-    item_genresEl.setAttribute("class", "item__description");
-    item_genresEl.textContent = "Genre: " + translateGenres(myMovie.genre_ids);
-
-    card_contentEl.append(item_titleEl);
-    card_contentEl.append(item_descriptionEl);
-    card_contentEl.append(item_genresEl);
-
+        card_contentEl.append(item_titleEl);
+        card_contentEl.append(item_descriptionEl);
+        card_contentEl.append(item_genresEl);
+    }
     cardEl.append(card_imageEl);
-    cardEl.append(card_contentEl);
+    if (myMovie)
+        cardEl.append(card_contentEl);
 
 
     sliderEl.append(cardEl);

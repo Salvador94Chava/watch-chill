3b86a3405d3c73738782f662c5848cfdc34e5123
abi <abimael.monarrez58@gmail.com>
2022-01-04 16:04:52 -0700

    limit genre selection

--- src/containers/genres/js/genres.js
+++ src/containers/genres/js/genres.js
@@ -3,6 +3,8 @@ var inputYes = document.querySelectorAll("input.yes");
 var inputNo = document.querySelectorAll("input.no");
 var label = document.querySelectorAll("label");
 var input = document.querySelectorAll("input");
+var genresChecked = 0;
+console.log(genresChecked);
 
 nextBtn.addEventListener("click", storeGenreOptions);
 
@@ -16,7 +18,33 @@ function uncheck(event) {
             inputNo[i].checked = false;
         } else if (event.target.className === "no" && event.target.dataset.index === inputYes[i].dataset.index && inputYes[i].checked === true) {
             inputYes[i].checked = false;
+            genresChecked = genresChecked - 1;
+            console.log(genresChecked);
+        } else if (genresChecked < 3) {
+            inputYes[i].removeAttribute("disabled");
         }
+    }
+}
+
+for (var i = 0; i < inputYes.length; i++) {
+    inputYes[i].addEventListener("click", disable);
+}
+
+function disable(event) {
+    if (event.target.checked) {
+        genresChecked = genresChecked + 1;
+        console.log(genresChecked);
+        console.log(event.target.checked);
+    } else if (!event.target.checked) {
+        genresChecked = genresChecked - 1;
+        console.log(genresChecked);
+        console.log(event.target.checked);
+    } 
+
+    for (var i = 0; i < inputYes.length; i++) {
+        if (genresChecked === 3 && !inputYes[i].checked) {
+            inputYes[i].setAttribute("disabled", "");
+        } 
     }
 }
 

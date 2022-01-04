0703b1efbdeffe90476e0467286b72ff0ec608e1
Guillermo Lazo <glazovg@gmail.com>
2022-01-03 22:17:41 -0600

    Adding href to app logo

--- src/containers/navigation-bar/js/nav.js
+++ src/containers/navigation-bar/js/nav.js
@@ -1,6 +1,8 @@
 let navBarEl = $('nav.nav-bar');
 let logoPath = location.pathname.includes('containers') ? '../../../assets/images/watch_chill_logo.png'
   : './assets/images/watch_chill_logo.png';
+let homePath = location.pathname.includes('containers') ? '../../../index.html'
+  : './index.html';
 
 
 navBarEl.addClass("navbar level");
@@ -8,7 +10,7 @@ navBarEl.attr("role", "navigation");
 navBarEl.attr("aria-label", "main navigation");
 navBarEl.html(`
 <div class="level-item">
-    <a href="">
+    <a href="${homePath}">
       <img src="${logoPath}" alt="Watch&Chill logo" style= "height: 150px;">
     </a>
   </div>
